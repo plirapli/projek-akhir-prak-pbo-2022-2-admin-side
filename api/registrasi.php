1ee1a7d93ae3d953827d2829262e049373ccb574
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-05-21 21:18:35 +0700

    add functionality

--- api/registrasi.php
+++ api/registrasi.php
@@ -6,51 +6,14 @@ if (function_exists($_GET['function'])) {
   $_GET['function']();
 }
 
-// // GET ORDER
-// function get_order()
-// {
-//   global $connection;
-
-//   $query = "SELECT pesanan.*, user.nama FROM pesanan LEFT JOIN user ON pesanan.id_user = user.id_user";
-//   $result = mysqli_query($connection, $query);
-
-//   if ($query) {
-//     while ($row = mysqli_fetch_object($result)) {
-//       $data[] = $row;
-//     }
-
-//     if (isset($data)) {
-//       $response = [
-//         'status' => 1,
-//         'message' => 'Success',
-//         'data' => $data
-//       ];
-//     } else {
-//       $response = [
-//         'status' => 1,
-//         'message' => 'Data Kosong',
-//         'data' => []
-//       ];
-//     }
-//   } else {
-//     $response = [
-//       'status' => 0,
-//       'message' => 'Error: ' . mysqli_error($connection),
-//     ];
-//   }
-
-//   header('Content-Type: application/json');
-//   echo json_encode($response);
-// }
-
-// GET ORDER BY ID
+// GET REGISTRASI BY REG. NUMBER
 function get_by_reg_num()
 {
   global $connection;
 
   $reg = $_GET["reg"];
   $query =
-    "SELECT kode_registrasi AS kode, u.nama AS user, d.nama AS dokter FROM registrasi r INNER JOIN user u ON r.id_user=u.id_user INNER JOIN dokter d ON r.id_dokter=d.id_dokter WHERE r.kode_registrasi = $reg";
+    "SELECT kode_registrasi AS kode, u.id_user AS id_user, u.nama AS user, d.id_dokter AS id_dokter, d.nama AS dokter FROM registrasi r INNER JOIN user u ON r.id_user=u.id_user INNER JOIN dokter d ON r.id_dokter=d.id_dokter WHERE r.kode_registrasi = $reg";
   $result = mysqli_query($connection, $query);
 
   if ($result) {
@@ -75,122 +38,108 @@ function get_by_reg_num()
       'message' => 'Failed',
     ];
   }
-
   header('Content-Type: application/json');
   echo json_encode($response);
 }
 
-// // POST ORDER
-// function add_order()
-// {
-//   global $connection;
+// GET LAST NOMOR ANTRIAN
+function get_last_antrian()
+{
+  global $connection;
 
-//   $req_body = json_decode(file_get_contents('php://input'), true);
-//   $id_user = $req_body["id_user"];
+  $id_dokter = $_GET["id_dokter"];
+  $query = "SELECT MAX(nomor_antrian) AS nomor FROM antrian WHERE id_dokter = $id_dokter";
 
-//   $query = "INSERT INTO pesanan 
-//             VALUE ( '', $id_user, current_timestamp())";
-//   $result = mysqli_query($connection, $query);
-//   $id = mysqli_query($connection, "SELECT LAST_INSERT_ID() AS id_order");
+  $result = mysqli_query($connection, $query);
 
-//   $data = mysqli_fetch_object($id);
+  if ($result) {
+    $data = mysqli_fetch_object($result);
 
-//   if ($result) {
-//     $response = [
-//       'status' => 1,
-//       'message' => 'Insert Success',
-//       'data' => $data,
-//     ];
-//   } else {
-//     $response = [
-//       'status' => 0,
-//       'message' => 'Error: ' . mysqli_error($connection)
-//     ];
-//   }
+    if (isset($data)) {
+      $response = [
+        'status' => 1,
+        'message' => 'Success',
+        'data' => $data
+      ];
+    } else {
+      $response = [
+        'status' => 0,
+        'message' => 'Data Kosong',
+        'data' => []
+      ];
+    }
+  } else {
+    $response = [
+      'status' => 0,
+      'message' => 'Failed',
+    ];
+  }
+  header('Content-Type: application/json');
+  echo json_encode($response);
+}
 
-//   header('Content-Type: application/json');
-//   echo json_encode($response);
-// }
+// ADD TABLE ANTRIAN
+function add_antrian()
+{
+  global $connection;
 
-// // GET TRANSACTION BY ID
-// function get_transaction_id()
-// {
-//   global $connection;
 
-//   $id_order = $_GET["id_order"];
-//   $query = "SELECT * FROM transaksi t WHERE t.id_pesanan = $id_order";
-//   $result = mysqli_query($connection, $query);
+  $check = [
+    'id_user' => '',
+    'id_dokter' => '',
+    'nomor' => '',
+  ];
+  $req_body = json_decode(file_get_contents('php://input'), true);
+  $check_match = count(array_intersect_key($req_body, $check));
 
-//   if ($query) {
-//     while ($row = mysqli_fetch_object($result)) {
-//       $data[] = $row;
-//     }
+  if ($check_match == count($check)) {
+    $id_user = $req_body['id_user'];
+    $id_dokter = $req_body['id_dokter'];
+    $nomor = $req_body['nomor'];
 
-//     if (isset($data)) {
-//       $response = [
-//         'status' => 1,
-//         'message' => 'Success',
-//         'data' => $data
-//       ];
-//     } else {
-//       $response = [
-//         'status' => 1,
-//         'message' => 'Data Kosong',
-//         'data' => []
-//       ];
-//     }
-//   } else {
-//     $response = [
-//       'status' => 0,
-//       'message' => 'Error: ' . mysqli_error($connection),
-//     ];
-//   }
+    $query = "INSERT INTO antrian(id_user, id_dokter, nomor_antrian) VALUES ($id_user, $id_dokter, $nomor)";
+    $result = mysqli_query($connection, $query);
 
-//   header('Content-Type: application/json');
-//   echo json_encode($response);
-// }
+    if ($result) {
+      $response = [
+        'status' => 1,
+        'message' => 'Insert Success',
+      ];
+    } else {
+      $response = [
+        'status' => 0,
+        'message' => 'Error: ' . mysqli_error($connection)
+      ];
+    }
+  } else {
+    $response = [
+      'status' => 0,
+      'message' => 'Wrong Parameter'
+    ];
+  }
+  header('Content-Type: application/json');
+  echo json_encode($response);
+}
 
-// // ADD TO TRANSACTION
-// function add_transaction()
-// {
-//   global $connection;
+// DELETE REGISTRASI BY REG NUM
+function delete_registrasi()
+{
+  global $connection;
 
-//   $check = [
-//     'id_order' => '',
-//     'menu' => '',
-//     'qty' => '',
-//     'harga' => '',
-//   ];
-//   $req_body = json_decode(file_get_contents('php://input'), true);
-//   $check_match = count(array_intersect_key($req_body, $check));
+  $reg = $_GET['reg'];
+  $query = "DELETE FROM registrasi WHERE kode_registrasi = $reg";
 
-//   if ($check_match == count($check)) {
-//     $id_order = $req_body['id_order'];
-//     $menu = $req_body['menu'];
-//     $qty = $req_body['qty'];
-//     $harga = $req_body['harga'];
-
-//     $command = "INSERT INTO transaksi 
-//                 VALUE ( '', '$id_order', '$menu', '$qty', $harga )";
-//     $query = mysqli_query($connection, $command);
-
-//     if ($query) {
-//       $response = [
-//         'status' => 1,
-//         'message' => 'Insert Success',
-//       ];
-//     } else {
-//       $response = [
-//         'status' => 0,
-//         'message' => 'Error: ' . mysqli_error($connection)
-//       ];
-//     }
-//   } else {
-//     $response = [
-//       'status' => 0,
-//       'message' => 'Wrong Parameter'
-//     ];
-//   }
-//   header('Content-Type: application/json');
-//   echo json_encode($response);
-// }
+  if (mysqli_query($connection, $query)) {
+    $response = [
+      'status' => 1,
+      'message' => 'Berhasil Meghapus'
+    ];
+  } else {
+    $response = [
+      'status' => 0,
+      'message' => mysqli_error($connection)
+    ];
+  }
+  header('Content-Type: application/json');
+  echo json_encode($response);
+}
--- helper/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$db = 'yulje_medical_centre';
-
-$connection = mysqli_connect($host, $username, $password, $db);
